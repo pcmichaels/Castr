4489a90797463e586cbc8657a17b53707f87a6f9
Paul Michaels <paul.michaels@musicmagpie.co.uk>
2021-04-27 15:41:00 +0100

    Add extension for AsDictionary

---
+++ Castr/Extensions/CastrClassExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Castr.Extensions
+{
+    public static class CastrExtensions
+    {
+        public static Dictionary<string, object> AsDictionary<T>(this T classToConvert)
+        {
+            var castr = new CastrClass<T>(classToConvert);
+            var dict = castr.AsDictionary();
+            return dict;
+        }
+    }
+}
